81ceb80ab113edaba74ce058f03de67aa46e4e9f
angelo.paolino <angelo.paolino@dataprev.gov.br>
2026-06-10 20:13:34 -0300

    tratativa erro escrita parte 3

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-# Garante que as pastas cruciais do Adianti existam e tenham as permissões corretas
-# (Caso o SQLite esteja em outro caminho, mude 'app/database' para o caminho correto)
+# Garante que as pastas cruciais do Adianti existam
 # mkdir -p app/database app/tmp app/output app/logs
 
-# Ajusta o dono das pastas para o usuário www-data (que internamente é o UID 1002)
+# Ajusta o proprietário para o www-data (UID 1002 / GID 1005)
 chown -R www-data:www-data app/database app/tmp app/output app/logs
 chmod -R 775 app/database app/tmp app/output app/logs
 
-# Executa o comando principal do container (que é o php-fpm)
-exec "$@"
+# Executa o comando principal (php-fpm) ALTERNANDO para o usuário www-data de forma segura
+exec gosu www-data "$@"
